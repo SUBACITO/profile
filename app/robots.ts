4cd5152ee8039fa8d8f188f6b96bb1154874bae5
SUBACITO <subacitodev@gmail.com>
2025-12-08 08:08:59 +0700

    update robots, sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,47 @@
 import { MetadataRoute } from "next";
 
-export default function robots() : MetadataRoute.Robots{
-    return{
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/'
-        },
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: 'Amazonbot',
+                disallow: '/',
+            },
+            {
+                userAgent: 'Applebot-Extended',
+                disallow: '/',
+            },
+            {
+                userAgent: 'Bytespider',
+                disallow: '/',
+            },
+            {
+                userAgent: 'CCBot',
+                disallow: '/',
+            },
+            {
+                userAgent: 'ClaudeBot',
+                disallow: '/',
+            },
+            {
+                userAgent: 'Google-Extended',
+                disallow: '/',
+            },
+            {
+                userAgent: 'GPTBot',
+                disallow: '/',
+            },
+            {
+                userAgent: 'meta-externalagent',
+                disallow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/private/', '/cra/', '/convert'],
+            },
+        ],
+
         sitemap: 'https://subacito.cf/sitemap.xml',
     }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,16 +3,16 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://subacito.cf/',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
+            url: 'https://subacito.cf',
+            lastModified: new Date("2025-12-08 12:39:20Z"),
+            changeFrequency: 'yearly',
             priority: 1,
         },
         {
             url: 'https://subacito.cf/about',
-            lastModified: new Date(),
+            lastModified: new Date("2025-12-08 15:39:40Z"),
             changeFrequency: 'monthly',
-            priority: 0.8,
+            priority: 0.5,
         },
         // thêm các URL khác nếu muốn
     ];
